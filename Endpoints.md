4cc85819dc920c17c70f036ec25c3c6ba3fa1f15
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-11-04 23:10:58 +0600

    auth done

---
+++ Endpoints.md
@@ -0,0 +1,6 @@
+# End Points
+
+| End Points            | Methods | Descriptions  |
+|-----------------------|---------|---------------|
+|/api/users/token-auth/ | post    |to obtain token|
+|http://127.0.0.1:8000/api/users/users/ | post , get | register, all users details|
--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import UserSerializer
 
-# Create your views here.
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username,
+            'email': user.email
+        })
